b646c8841bec99bfb2ba47b15c44633d80f1bdf8
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-23 10:16:00 -0600

    refactor pt3

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -3,36 +3,46 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class CohortTest {
     Cohort kuwan;
     Student ku;
+    Student da;
     //creates classes
     @Before
-    public void setup(){
-        kuwan =new Cohort();
-        ku =new Student(1l,"kuwan");
+    public void setup() {
+        kuwan = new Cohort();
+        ku = new Student(1l, "kuwan");
+        da = new Student(1l, "da");
     }
-    @Test
-    public void testAddStudent(){
-kuwan.addStudent(ku);
-ku.addGrade(90);
-assertEquals("kuwan",ku.getName() );
-}
 
-@Test
-    public void testToGetList(){
-        kuwan.getStudents();
-        assertNotNull(kuwan);
-}
-@Test
-    public void getTheAverage(){
-    kuwan.addStudent(ku);
-    ku.addGrade(90);
-    ku.addGrade(80);
-        kuwan.getCohortAverage();
-        assertEquals(85,kuwan.getCohortAverage(),0);
-}
+    @Test
+    public void testAddStudent() {
+        kuwan.addStudent(ku);
+        assertEquals(1, kuwan.getStudents().size());
     }
+
+    @Test
+    public void testToGetList() {
+
+        kuwan.addStudent(ku);
+        assertEquals(1,kuwan.getStudents().get(0).getId());
+        kuwan.addStudent(da);
+        assertEquals(1,kuwan.getStudents().get(1).getId());
+
+    }
+
+    @Test
+    public void getTheAverage() {
+        kuwan.addStudent(ku);
+        kuwan.addStudent(da);
+        ku.addGrade(100);
+        ku.addGrade(40);
+        da.addGrade(80);
+        da.addGrade(90);
+        assertEquals(77.5, kuwan.getCohortAverage(), 0);
+    }
+}
 
