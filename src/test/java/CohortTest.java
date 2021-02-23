0ae7db7a9b718f78ed13618d5c6c5c5012eede84
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-23 10:22:08 -0600

    finally refactor

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -16,32 +16,27 @@ public class CohortTest {
         kuwan = new Cohort();
         ku = new Student(1l, "kuwan");
         da = new Student(1l, "da");
-    }
-
-    @Test
-    public void testAddStudent() {
-        kuwan.addStudent(ku);
-        assertEquals(1, kuwan.getStudents().size());
-    }
-
-    @Test
-    public void testToGetList() {
-
-        kuwan.addStudent(ku);
-        assertEquals(1,kuwan.getStudents().get(0).getId());
-        kuwan.addStudent(da);
-        assertEquals(1,kuwan.getStudents().get(1).getId());
-
-    }
-
-    @Test
-    public void getTheAverage() {
         kuwan.addStudent(ku);
         kuwan.addStudent(da);
         ku.addGrade(100);
         ku.addGrade(40);
         da.addGrade(80);
         da.addGrade(90);
+    }
+
+    @Test
+    public void testAddStudent() {
+        assertEquals(2, kuwan.getStudents().size());
+    }
+    @Test
+    public void testToGetList() {
+        assertEquals(1,kuwan.getStudents().get(0).getId());
+        assertEquals(1,kuwan.getStudents().get(1).getId());
+
+    }
+    @Test
+    public void getTheAverage() {
+
         assertEquals(77.5, kuwan.getCohortAverage(), 0);
     }
 }
